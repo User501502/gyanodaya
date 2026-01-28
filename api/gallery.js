68e12455280e9f29e8d49a113da3d2bf9e0eb56b
smsvedang <smsvedang@gmail.com>
2026-01-28 09:52:01 +0530

    update admin gallery yt

--- api/gallery.js
+++ api/gallery.js
@@ -10,31 +10,43 @@ const GallerySchema = new mongoose.Schema({
 
 const Gallery = mongoose.models.Gallery || mongoose.model("Gallery", GallerySchema);
 
+function getEmbedUrl(url) {
+    if (!url) return url;
+    if (url.includes("youtube.com/embed/")) return url;
+
+    let videoId = "";
+    if (url.includes("v=")) {
+        videoId = url.split("v=")[1].split("&")[0];
+    } else if (url.includes("youtu.be/")) {
+        videoId = url.split("be/")[1].split("?")[0];
+    } else if (url.includes("shorts/")) {
+        videoId = url.split("shorts/")[1].split("?")[0];
+    } else if (url.includes("youtube.com/embed/")) {
+        videoId = url.split("embed/")[1].split("?")[0];
+    }
+
+    return videoId ? `https://www.youtube.com/embed/${videoId}` : url;
+}
+
 export default async function handler(req, res) {
     await connectDB();
 
     if (req.method === "GET") {
-        const images = await Gallery.find().sort({ createdAt: -1 });
-        return res.json(images);
+        const items = await Gallery.find().sort({ createdAt: -1 });
+        const processedItems = items.map(item => {
+            const doc = item.toObject();
+            if (doc.type === "video") {
+                doc.url = getEmbedUrl(doc.url);
+            }
+            return doc;
+        });
+        return res.json(processedItems);
     }
 
     if (req.method === "POST") {
         const data = req.body;
-
-        // Auto convert YouTube URL to embed URL if it's a video
-        if (data.type === "video" && data.url) {
-            let videoId = "";
-            if (data.url.includes("v=")) {
-                videoId = data.url.split("v=")[1].split("&")[0];
-            } else if (data.url.includes("be/")) {
-                videoId = data.url.split("be/")[1].split("?")[0];
-            } else if (data.url.includes("embed/")) {
-                videoId = data.url.split("embed/")[1].split("?")[0];
-            }
-
-            if (videoId) {
-                data.url = `https://www.youtube.com/embed/${videoId}`;
-            }
+        if (data.type === "video") {
+            data.url = getEmbedUrl(data.url);
         }
 
         const item = new Gallery(data);
